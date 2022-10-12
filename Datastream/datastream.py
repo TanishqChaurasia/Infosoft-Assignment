b4271ba97985cb159d4117d76a137d786efdc28f
Tanishq <tanishqchaurasia@gmail.com>
2022-10-12 22:06:15 +0530

    Added new line at EOF

--- Datastream/datastream.py
+++ Datastream/datastream.py
@@ -21,3 +21,4 @@ data_stream.should_output_data_str(timestamp = 1, data_string = "world")
 data_stream.should_output_data_str(timestamp = 6, data_string = "hello")
 data_stream.should_output_data_str(timestamp = 7, data_string = "hello")
 data_stream.should_output_data_str(timestamp = 8, data_string = "world")
+
